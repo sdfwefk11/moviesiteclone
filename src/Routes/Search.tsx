4b76813a6b22fa81896c6bc87bbec373999bc687
sdfwefk11 <mknhsad3398@naver.com>
2023-06-17 23:00:58 +0900

    URLSearchParams

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -2,7 +2,8 @@ import { useLocation } from "react-router-dom";
 
 function Search() {
   const location = useLocation();
-  console.log(location);
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
   return null;
 }
 
